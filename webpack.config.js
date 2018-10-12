49f6553a1d96bed086f9e29b47be9428551db008
brianotoole <botoole518@gmail.com>
2018-10-12 14:40:49 -0400

    fix file-loader path for relative images/fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,75 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: "./src/js/index.js",
   output: {
-    filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.(css|sass|scss)$/,
         use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'postcss-loader', 'sass-loader'],
+          use: ["css-loader", "postcss-loader", "sass-loader"]
         })
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
+        test: /\.(png|jpg|gif|svg)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./img/",
+              publicPath: "../img/"
+            }
+          }
+        ]
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./css/fonts/",
+              publicPath: "../"
+            }
+          }
+        ]
       },
       {
         test: /\.(csv|tsv)$/,
-        use: ['csv-loader']
+        use: ["csv-loader"]
       },
       {
         test: /\.xml$/,
-        use: ['xml-loader']
+        use: ["xml-loader"]
       }
     ]
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: 'css/style.css',
-      allChunks: true,
+      filename: "css/style.css",
+      allChunks: true
     }),
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
-    //title: 'Development'
+      //title: 'Development'
     }),
     new BrowserSyncPlugin({
       notify: false, // hide the notification
-      host: 'localhost',
+      host: "localhost",
       port: 3000,
-      proxy: 'http://localhost/didyournflteamwin',
-      files: [
-            '**/*.html'
-        ]
-    }),
-  ],
+      proxy: "http://localhost/didyournflteamwin",
+      files: ["**/*.html"]
+    })
+  ]
 };

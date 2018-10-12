c2bfc425d53003e0a51b92c487e91498e1cae8b8
brianotoole <botoole518@gmail.com>
2018-10-12 13:23:20 -0400

    install npm + configure webpack + packages

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+// extract text from webpack to bundled output file
+require("../scss/main.scss");
+
+import './scores.js';
+
+// test json data
+//import './test-data.json';
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'dist/css/style.css',
+      allChunks: true,
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+    //title: 'Development'
+    }),
+    new BrowserSyncPlugin({
+      notify: false, // hide the notification
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost/didyournflteamwin',
+      files: [
+            '**/*.html'
+        ]
+    }),
+  ],
+};
